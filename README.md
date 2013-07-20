d5a1e23434b21973e796dd6803065b7b4443d80d
Andrew Chilton <andychilton@gmail.com>
2013-07-20 17:43:27 +1200

    First go at rustle

---
+++ README.md
@@ -0,0 +1,71 @@
+# Rustle #
+
+Rustle is a stats aggregation library which can use a number of backends for storing a fixed-size database. It is
+similar in design to RRD and Whisper but uses Redis, Files, Memory or any number of other backends.
+
+Each of these backends can stream an entire fixed-size database hence can be written to disk, over the network
+or indeed to anything in Node which is a ```writeStream``` that can accept a ```.pipe()``` from a ```readStream```.
+
+## Example ##
+
+Imagine we wanted to keep a count of the number of hits on our homepage every minute and retain all stats for one
+year. We'd set up a stat such as the following:
+
+```
+var redis = require('redis');
+var rustle = require('./rustle.js');
+
+var client = redis.createClient();
+
+var homepageHits = rustle({
+    client       : client,
+
+    // keys are prefixed : "<domain>:<category>:<name>"
+    domain       : 'cssminifier',      //
+    category     : 'hits',             // 
+    name         : 'homepage',         // 
+    period       : 60,                 // one minute
+    retention    : 365 * 24 * 60 * 60, // one year
+    aggregation  : 'sum',              // average, sum, last, max, min
+});
+
+To increment the number of hits, just call ```hit```:
+
+homepageHits.inc(function(err) {
+   // ...
+});
+```
+
+If you'd like to know which periods have info:
+
+```
+homepageHits.keys(function(err, keys) {
+   // ...
+});
+```
+
+If you'd like to know get all the periods and their valies:
+
+```
+homepageHits.values(function(err, periods) {
+   // ...
+});
+```
+
+## ToDo ##
+
+Other commands such as the ability to get all the values between two time periods along with a periodLength to allow
+for more aggregation.
+
+## Author ##
+
+Written by [Andrew Chilton](http://chilts.org/) - [Blog](http://chilts.org/blog/) -
+[Twitter](https://twitter.com/andychilton).
+
+## License ##
+
+Copyright © 2013 Andrew Chilton <andychilton@gmail.com>.  All rights reserved.
+
+See [http://chilts.mit-license.org/2013/](http://chilts.mit-license.org/2013/).
+
+(Ends)
---
+++ rustle.js
@@ -0,0 +1,165 @@
+// ----------------------------------------------------------------------------
+//
+//
+//
+//
+// ----------------------------------------------------------------------------
+
+var util = require('util');
+
+// ----------------------------------------------------------------------------
+
+function create(opts) {
+    // check we have a client
+    if ( !opts.client ) {
+        throw new Error("rustle: Provide a Redis client");
+    }
+
+    // check we have a category
+    if ( typeof opts.category !== 'string' ) {
+        throw new Error("rustle: Provide a valid category");
+    }
+
+    // check we have a domain
+    if ( typeof opts.domain !== 'string' ) {
+        throw new Error("rustle: Provide a valid domain");
+    }
+
+    // check we have a name
+    if ( typeof opts.name !== 'string' ) {
+        throw new Error("rustle: Provide a valid name");
+    }
+
+    // check we have a valid period
+    if ( typeof opts.period !== 'number' ) {
+        throw new Error("rustle: Provide a valid period");
+    }
+
+    // check we have a valid retention
+    if ( typeof opts.retention !== 'number' ) {
+        throw new Error("rustle: Provide a valid period");
+    }
+
+    // now create the correct class for this aggregation type
+    if ( opts.aggregation === 'sum' ) {
+        return new Sum(opts);
+    }
+
+    throw new Error("rustle: Unknown aggregation type : " + opts.aggregation);
+}
+
+function Sum(opts) {
+    var self = this;
+
+    // save these options locally
+    self.domain    = opts.domain;
+    self.category  = opts.category;
+    self.name      = opts.name;
+    self.period    = opts.period;
+    self.retention = opts.retention;
+
+    // save the redis client
+    self.client    = opts.client;
+
+    // return this
+    return self;
+}
+
+// this just returns some meta info about this stat (not about the stats itself)
+Sum.prototype.info = function() {
+    var self = this;
+    return {
+        aggregation : 'sum',
+        name        : self.name,
+        domain      : self.domain,
+        category    : self.category,
+        retention   : self.retention,
+        period      : self.period,
+        total       : self.retention * self.period,
+    };
+}
+
+// this hits the client to find out more info related to this stat
+Sum.prototype.stats = function(callback) {
+    var self = this;
+    process.nextTick(function() {
+        callback(null, {});
+    });
+}
+
+Sum.prototype.inc = function(timestamp, callback) {
+    var self = this;
+
+    if ( typeof callback == 'undefined' ) {
+        callback = timestamp;
+        timestamp = Date.now();
+    }
+
+    // ok, firstly, round down the timestamp so it is on the previous period boundary
+    timestamp = Math.floor(timestamp / 1000);
+    var thisPeriod = timestamp - ( timestamp % self.period );
+
+    var periodKey = [ self.domain, self.category, self.name, thisPeriod ].join(':');
+    var setName = [ self.domain, self.category, self.name ].join(':');
+
+    // finally, hit the client with this stuff
+    self.client
+        .multi()
+        .zadd(setName, thisPeriod, periodKey)
+        .incr(periodKey)
+        .exec(function(err, results) {
+            callback(err, results);
+        })
+    ;
+}
+
+Sum.prototype.keys = function(callback) {
+    var self = this;
+
+    // callback = callback || function(){};
+
+    // zrange cssminifier:hits:homepage 0 -1
+    var setName = [ self.domain, self.category, self.name ].join(':');
+    this.client.zrange(setName, 0, -1, function(err, keys) {
+        if (err) return callback(err);
+        keys.forEach(function(key, i) {
+            var bits = key.split(':');
+            keys[i] = bits[3];
+        });
+        callback(null, keys);
+    });
+};
+
+Sum.prototype.values = function(callback) {
+    var self = this;
+
+    // callback = callback || function(){};
+
+    // zrange cssminifier:hits:homepage 0 -1
+    var setName = [ self.domain, self.category, self.name ].join(':');
+    this.client.sort(setName, 'by', 'scores', 'get', '#', 'get', '*', function(err, values) {
+        if (err) return callback(err);
+        var vals = [];
+        var key;
+        values.forEach(function(v) {
+            if ( key ) {
+                var bits = key.split(':');
+                vals.push({
+                    ts   : bits[3],
+                    val  : v
+                });
+                key = undefined;
+            }
+            else {
+                key = v;
+            }
+        });
+        callback(null, vals);
+    });
+};
+
+// ----------------------------------------------------------------------------
+
+module.exports = create;
+
+// ----------------------------------------------------------------------------
